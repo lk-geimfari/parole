4e276d94accdaefe932b997d3e50d1889dd967bd
Isaak Uchakaev <likid.geimfari@gmail.com>
2020-08-22 19:11:18 +0300

    Add fmt plugin for rebar3

--- src/parole.erl
+++ src/parole.erl
@@ -20,60 +20,60 @@
 %%====================================================================
 
 start() ->
-  gen_server:start_link({global, ?MODULE}, ?MODULE, [], []).
+    gen_server:start_link({global, ?MODULE}, ?MODULE, [], []).
 
 add(Resource, Password) when is_binary(Password) ->
-  gen_server:call({global, ?MODULE}, {add, Resource, Password}).
+    gen_server:call({global, ?MODULE}, {add, Resource, Password}).
 
 delete(Resource) ->
-  gen_server:cast({global, ?MODULE}, {delete, Resource}).
+    gen_server:cast({global, ?MODULE}, {delete, Resource}).
 
 lookup(Resource) ->
-  gen_server:call({global, ?MODULE}, {lookup, Resource}).
+    gen_server:call({global, ?MODULE}, {lookup, Resource}).
 
 get(Resource) ->
-  gen_server:call({global, ?MODULE}, {get, Resource}).
+    gen_server:call({global, ?MODULE}, {get, Resource}).
 
 get(Resource, decrypted) ->
-  gen_server:call({global, ?MODULE}, {get, Resource, decrypted}).
+    gen_server:call({global, ?MODULE}, {get, Resource, decrypted}).
 
 %%====================================================================
 %% Callback Functions
 %%====================================================================
 
 init(_Args) ->
-  State = gb_trees:empty(),
-  {ok, State}.
+    State = gb_trees:empty(),
+    {ok, State}.
 
 handle_call({add, Resource, Password}, _From, State) ->
-  EncryptedPassword = base64:encode(Password),
-  NewState = gb_trees:insert(Resource, EncryptedPassword, State),
-  {reply, ok, NewState};
+    EncryptedPassword = base64:encode(Password),
+    NewState = gb_trees:insert(Resource, EncryptedPassword, State),
+    {reply, ok, NewState};
 handle_call({lookup, Resource}, _From, State) ->
-  Response = case gb_trees:lookup(Resource, State) of
-               {value, _} -> true;
-               _ -> not_found
-             end,
-  {reply, Response, State};
+    Response = case gb_trees:lookup(Resource, State) of
+                   {value, _} -> true;
+                   _ -> not_found
+               end,
+    {reply, Response, State};
 handle_call({get, Resource}, _From, State) ->
-  Response = gb_trees:get(Resource, State),
-  {reply, Response, State};
+    Response = gb_trees:get(Resource, State),
+    {reply, Response, State};
 handle_call({get, Resource, decrypted}, _From, State) ->
-  Response = gb_trees:get(Resource, State),
-  DecryptedResponse = base64:decode(Response),
-  {reply, DecryptedResponse, State}.
+    Response = gb_trees:get(Resource, State),
+    DecryptedResponse = base64:decode(Response),
+    {reply, DecryptedResponse, State}.
 
 handle_cast({delete, Resource}, State) ->
-  NewState = gb_trees:delete(Resource, State),
-  {noreply, NewState};
+    NewState = gb_trees:delete(Resource, State),
+    {noreply, NewState};
 handle_cast(_Request, State) ->
-  {noreply, State}.
+    {noreply, State}.
 
 handle_info(_Info, State) ->
-  {noreply, State}.
+    {noreply, State}.
 
 terminate(_Reason, State) ->
-  {ok, State}.
+    {ok, State}.
 
 code_change(_OldVsn, State, _Extra) ->
-  {ok, State}.
+    {ok, State}.
